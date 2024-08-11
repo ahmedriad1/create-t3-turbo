b2b47d0b9c78b336897cb194ebd5ffe2e08f2afb
ahmedriad1 <ahmed.riad.elashrey@gmail.com>
2024-08-11 11:54:51 +0300

    remove expo & restructure monorepo

---
+++ apps/web/next.config.js
@@ -0,0 +1,37 @@
+import { fileURLToPath } from "url";
+import createJiti from "jiti";
+
+// TODO: uncomment when adding Prisma
+// import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
+
+// Import env files to validate at build time. Use jiti so we can load .ts files in here.
+createJiti(fileURLToPath(import.meta.url))("./src/env");
+
+/** @type {import("next").NextConfig} */
+const config = {
+  reactStrictMode: true,
+
+  /** Enables hot reloading for local packages without a build step */
+  transpilePackages: [
+    // "@acme/api",
+    // "@acme/auth",
+    // "@acme/db",
+    // "@acme/ui",
+    // "@acme/validators",
+  ],
+
+  /** We already do linting and typechecking as separate tasks in CI */
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+
+  // TODO: uncomment when adding Prisma
+  // webpack: (config, { isServer }) => {
+  //   if (isServer) {
+  //     config.plugins = [...config.plugins, new PrismaPlugin()];
+  //   }
+
+  //   return config;
+  // },
+};
+
+export default config;
---
+++ apps/web/src/server/db.ts
@@ -0,0 +1,19 @@
+// TODO: Uncomment the code below and replace the `@acme/db` import with the correct path to the Prisma client.
+
+// import { env } from "@/env";
+
+// import { PrismaClient } from "@acme/db";
+
+// const createPrismaClient = () =>
+//   new PrismaClient({
+//     log:
+//       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+//   });
+
+// const globalForPrisma = globalThis as unknown as {
+//   prisma: ReturnType<typeof createPrismaClient> | undefined;
+// };
+
+// export const db = globalForPrisma.prisma ?? createPrismaClient();
+
+// if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
